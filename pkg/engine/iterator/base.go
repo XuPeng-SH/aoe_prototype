7bdf86137887ddb515dcca739ad9bb0ea28da0c9
XuPeng-SH <xupeng3112@163.com>
2021-05-16 23:53:47 +0800

    update iterator

--- pkg/engine/iterator/base.go
+++ pkg/engine/iterator/base.go
@@ -1,15 +1,58 @@
 package it
 
+type BaseResources struct {
+	Impl IResources
+}
+
 type BaseIterator struct {
 	Err       error
 	Result    interface{}
 	Executor  ExecutorT
 	Resources IResources
+	Impl      Iterator
 }
 
 var (
-	_ Iterator = (*BaseIterator)(nil)
+	_ IResources = (*BaseResources)(nil)
+	_ Iterator   = (*BaseIterator)(nil)
 )
+
+func NewBaseResources(impl IResources) IResources {
+	res := &BaseResources{
+		Impl: impl,
+	}
+	return res
+}
+
+func NewBaseIterator(impl Iterator, executor ExecutorT, resources IResources) Iterator {
+	iter := &BaseIterator{
+		Executor:  executor,
+		Resources: resources,
+		Impl:      impl,
+	}
+	return iter
+}
+
+func (res *BaseResources) IterResource(iter Iterator) {
+	err := iter.PreIter()
+	if err != nil {
+		iter.SetErr(err)
+		return
+	}
+
+	err = res.Impl.HandleResources(iter.Execute)
+	if err != nil {
+		iter.SetErr(err)
+		return
+	}
+
+	err = iter.PreIter()
+	iter.SetErr(err)
+}
+
+func (res *BaseResources) HandleResources(handle HandleT) error {
+	return nil
+}
 
 func (iter *BaseIterator) SetErr(err error) {
 	iter.Err = err
@@ -41,7 +84,7 @@ func (iter *BaseIterator) PostIter() error {
 
 func (iter *BaseIterator) Execute(res interface{}) error {
 	if iter.Executor != nil {
-		return iter.Executor(res, iter)
+		return iter.Executor(res, iter.Impl)
 	}
 	return nil
 }
--- pkg/engine/iterator/types.go
+++ pkg/engine/iterator/types.go
@@ -3,9 +3,11 @@ package it
 import ()
 
 type ExecutorT func(interface{}, Iterator) error
+type HandleT func(interface{}) error
 
 type IResources interface {
 	IterResource(Iterator)
+	HandleResources(HandleT) error
 }
 
 type Iterator interface {
